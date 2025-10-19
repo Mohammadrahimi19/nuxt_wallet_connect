9a0412c6724d0cabe4e147d6783b1bdd9abecd83
M.Rahimi <mmadrhimi19@gmail.com>
2025-10-19 23:04:49 +0330

    initial:setup base project

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2 class="text-3xl text-center bg-white w-full">hi im here !</h2>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+<style>
+@import "tailwindcss";
+
+</style>
